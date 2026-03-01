35e7ea7008bd393f9f08033ca8b7305d62db0d24
Aarogya <aarufloyd@gmail.com>
2026-03-01 22:26:50 +0545

    feat: add update business API with tests too

--- backend/src/routes/v1/business.ts
+++ backend/src/routes/v1/business.ts
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getBusiness,findBusinessById,deleteBusiness } = require('../../controllers/v1/businessController');
+const { getBusiness,findBusinessById,deleteBusiness,updateBusiness } = require('../../controllers/v1/businessController');
 
 const router = express.Router();
 
 router.get('/', getBusiness);
 router.get('/:id', findBusinessById);
 router.delete('/:id', deleteBusiness);
+router.put('/:id',updateBusiness );
 
 
 module.exports = router;
